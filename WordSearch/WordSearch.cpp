9ed96512304aa1a6234f31483ddb66b441829057
Nikita Dashkovskiy <misternikita16@mail.ru>
2021-10-08 09:16:18 +0800

    Добавьте файлы проекта.

---
+++ WordSearch/WordSearch.cpp
@@ -0,0 +1,20 @@
+﻿#include<iostream>
+#include<string>
+#include<sstream>
+#include<map>
+using namespace std;
+
+int main() {
+	system("chcp 1251");
+	string text;
+	getline(cin, text);
+	istringstream stream(text);
+	map<string, int> map;
+	for (string word; stream >> word; ++map[word]);
+	pair<string,int> pair;
+	for (auto element : map) {
+		if (pair.second < element.second) pair = element;
+	}
+	cout << "Наиболее встречаемое слово (" << pair.first << ") встретилось " << pair.second << " раз(а)" << endl;
+
+}
